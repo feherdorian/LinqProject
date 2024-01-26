285d563181b606b6fba2726d2a72980dd5ab61bd
BigFeri <ferenc0516@gmail.com>
2024-01-26 10:18:22 +0100

    -

--- LinqProjectek/LinqProjekt_TöbbTáblás/LinqProjekt/Program.cs
+++ LinqProjectek/LinqProjekt_TöbbTáblás/LinqProjekt/Program.cs
@@ -10,17 +10,7 @@ namespace LinqProjekt
     {
         static void Main(string[] args)
         {
-            string fajl = "focicsapat.txt";
-            Bemutato b = new Bemutato(fajl);
 
-            Console.WriteLine("1. Csapatok névsorban");
-            b.Nevsor();
-
-            Console.WriteLine("\n2. 20 vagy több bajnoki címmel rendelkező csapatok");
-            b.SokCimesek();
-
-            Console.WriteLine("3. Országonként hány csapat van");
-            b.OrszagonkentCsapatok();
 
             Console.ReadLine();
         }
